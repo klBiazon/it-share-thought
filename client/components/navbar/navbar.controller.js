8461d0f4757476860c3f65e1eb0f3fc42838fc83
klBiazon <klbiazon@dynamicobjx.com>
2016-12-16 17:22:52 +0800

    Modified navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,12 +4,30 @@ class NavbarController {
   //end-non-standard
 
   //start-non-standard
-  constructor(Auth) {
+  constructor(Auth, $state) {
+    this.$state = $state;
+    this.currentState = $state.current.name;
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
     this.getCurrentUser = Auth.getCurrentUser;
   }
 
+  login() {
+    this.currentState = 'login';
+    this.$state.go('login');
+    // angular.element(document.querySelector('.nav-tabs > li')).removeClass('active');
+  }
+
+  logout() {
+    this.$state.go('logout');
+    angular.element(document.querySelector('#home')).addClass('active');
+    this.currentState = 'main';
+  }
+
+  navigate(state) {
+    this.$state.go(state);
+    this.currentState = state;
+  }
 }
 
 angular.module('itShareThoughtApp')
