cf7c2a96b39083b90540f3ce9c8070d6cca2c080
klBiazon <klbiazon@dynamicobjx.com>
2016-12-16 17:22:38 +0800

    changed fields on post

--- server/api/post/post.model.js
+++ server/api/post/post.model.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 
 var PostSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  body:  { type: String, required: true },
-  canComment: { type: Boolean, default: true }
+  content:  { type: String, required: true },
+  canComment: { type: Boolean },
   isActive: { type: Boolean, default: true }
 }, {
   timestamps: true
